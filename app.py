d9577ae7a96c36400e3ab4ca9a0d4a717b4c23b0
sahilmahadik7 <sahilmahadik462@gmail.com>
2025-06-22 13:09:36 +0530

    Changes

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def internal_error(error):
     return jsonify({"error": "Internal server error"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True ,host="0.0.0.0", port=8080)
